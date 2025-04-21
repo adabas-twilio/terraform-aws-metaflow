cda6ef6920d192bbe2c188c9b50188db1082618a
Ajay Dabas <adabas@twilio.com>
2025-04-21 14:05:52 +0530

    fix: bump to latest release of metadata service and ui

--- modules/common/locals.tf
+++ modules/common/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  default_metadata_service_container_image = "netflixoss/metaflow_metadata_service:v2.4.12"
-  default_ui_static_container_image        = "public.ecr.aws/outerbounds/metaflow_ui:v1.2.4"
+  default_metadata_service_container_image = "netflixoss/metaflow_metadata_service:v2.5.0"
+  default_ui_static_container_image        = "public.ecr.aws/outerbounds/metaflow_ui:v1.3.13"
 }
