688414b784d81b76e8592161e70b9f06ded670bc
Ajay Dabas <adabas@twilio.com>
2025-09-29 21:37:03 +0530

    fix: remove KMS

--- modules/datastore/s3.tf
+++ modules/datastore/s3.tf
@@ -2,14 +2,6 @@ resource "aws_s3_bucket" "this" {
   bucket        = local.s3_bucket_name
   acl           = "private"
   force_destroy = var.force_destroy_s3_bucket
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.s3.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 
   tags = merge(
     var.standard_tags,
